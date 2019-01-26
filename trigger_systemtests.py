fd297604745df91b693e3dc13a0f3a1e76042485
shkodm <dmytro.sashko@tum.de>
2019-01-26 22:00:22 +0100

    Add option to exit only when triggered job is done

--- trigger_systemtests.py
+++ trigger_systemtests.py
@@ -7,6 +7,8 @@
 
 import json
 import os
+import time
+from sys import exit
 import argparse
 from string import Template
 from urllib.parse import urlencode
@@ -22,13 +24,35 @@ nm_test_map = {'openfoam': ['of-of', 'of-ccx'],
         'su2': ['su2-ccx'] }
 
 
-def generate_travis_job(adapter, user):
+def get_json_response(url, **kwargs):
+
+    headers = {
+      "Content-Type" : "application/json",
+      "Accept": "application/json",
+      "Travis-API-Version": "3",
+      "Authorization": "token {}".format(os.environ['TRAVIS_ACCESS_TOKEN'])
+    }
+
+    req = Request(url, headers = headers, **kwargs )
+    response = urlopen( req ).read().decode()
+    json_response = json.loads( response )
+
+    return json_response
+
+def generate_travis_job(adapter, user, trigger_failure = True):
+
+    triggered_by = os.environ["TRAVIS_JOB_WEB_URL"] if "TRAVIS_JOB_WEB_URL" in\
+         os.environ else "manual script call"
+
+    after_failure_action = "python push.py -t $TEST;"
+    if trigger_failure:
+        after_failure_action += " python trigger_systemtests.py --failure --owner $USER --adapter $ADAPTER"
 
     job_templates= {
         "name":  Template('[16.04] $TESTNAME <-> $TESTNAME'),
         "script": Template('python system_testing.py -s $TEST') ,
         "after_success":Template('python push.py -s -t $TEST') ,
-        "after_failure": Template("python push.py -t $TEST; python trigger_systemtests.py --failure --owner $USER --adapter $ADAPTER")}
+        "after_failure": Template(after_failure_action)}
 
     job_body={
         "request": {
@@ -60,7 +84,7 @@ def generate_travis_job(adapter, user):
                 ADAPTER=adapter)
         jobs.append(job)
 
-    job_body["request"]["message"] = adapter + ' systemtest'
+    job_body["request"]["message"] = "{} systemtest. Triggered by:{}".format(adapter, triggered_by)
     job_body["request"]["config"]["jobs"]["include"] = jobs;
 
     return job_body
@@ -74,16 +98,64 @@ def trigger_travis_build(job_body, user, repo):
     url = "https://api.{TRAVIS_URL}/repo/{USER}%2F{REPO}/requests".format(TRAVIS_URL="travis-ci.org",
         USER=user,REPO=repo)
 
-    headers = {
-      "Content-Type" : "application/json",
-      "Accept": "application/json",
-      "Travis-API-Version": "3",
-      "Authorization": "token {}".format(os.environ['TRAVIS_ACCESS_TOKEN'])
-    }
-
     data = json.dumps(job_body).encode('utf8')
-    req = Request(url, headers = headers, data = data)
-    response = urlopen(req ).read().decode()
+
+    return get_json_response(url, data = data)
+
+def check_job_status(job_id):
+    """ Checks status of the travis job"""
+    url = "https://api.{TRAVIS_URL}/build/{JOB_ID}".format(TRAVIS_URL="travis-ci.org", JOB_ID=job_id)
+
+    resp =  get_json_response(url)
+    return resp['state']
+
+def get_requests(user, repo):
+
+    url = "https://api.{TRAVIS_URL}/repo/{USER}%2F{REPO}/requests".format(TRAVIS_URL="travis-ci.org",
+            USER=user, REPO=repo )
+    return get_json_response(url)
+
+def query_request_info(user, repo, req_id):
+
+    url = "https://api.{TRAVIS_URL}/repo/{USER}%2F{REPO}/request/{REQUEST_ID}".format(TRAVIS_URL="travis-ci.org",
+            USER=user, REPO=repo, REQUEST_ID=req_id)
+    return get_json_response(url)
+
+def trigger_travis_and_wait_and_respond(job_body, user, repo):
+
+    json_response = trigger_travis_build(job_body, user, repo)
+    request_id = json_response['request']['id']
+
+    request_info = query_request_info(user, repo, request_id)
+    # it case request is being processes slow for whatever reasons
+    # (e.g) multiple requests were triggered, we don't yet have
+    # info about job_id, so we'll wait until we have it
+    print ("Request pending..")
+    while request_info.get('state') == 'pending':
+        # for ther reference in case of failures
+        print ("Current request status is {}".format(request_info['state']))
+        request_info = query_request_info(user, repo, request_id)
+        time.sleep(60)
+
+    if request_info["result"] != "approved":
+        raise Exception("Systemtest build request did not get approved")
+
+    job_id = request_info['builds'][0]['id']
+
+    job_status = ''
+    success_status = ["passed", "canceled"]
+    failed_status = ["errored", "failed"]
+
+    print ("Job started..")
+    while not job_status in (success_status + failed_status):
+        job_status = check_job_status(job_id)
+        print ("Current job status is {}. Be patient...".format(job_status))
+        time.sleep(60)
+
+    if job_status in success_status:
+        exit(0)
+    else:
+        exit(1)
 
 
 def generate_failure_callback():
@@ -91,9 +163,11 @@ def generate_failure_callback():
     running anything
     """
 
+    triggered_by = os.environ["TRAVIS_JOB_WEB_URL"];
+
     callback_body={
     "request": {
-      "message": "Systemtests failed",
+     "message": "Systemtests failed. Build url:{}".format(triggered_by),
       "branch": "master",
         "config": {
           "merge_mode": "replace",
@@ -114,16 +188,22 @@ def generate_failure_callback():
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Generate and trigger job for systemtests")
-    parser.add_argument('--owner',  type=str, help="Owner of repository", default='precice' )
+    parser.add_argument('--owner',  type=str, help="Owner of repository", default='shkodm' )
     parser.add_argument('--adapter', type=str, help="Adapter for which you want to trigger systemtests",
               required=True, choices = ["openfoam", "su2", "calculix"])
     parser.add_argument('--failure', help="Whether to trigger normal or failure build",
               action="store_true")
+    parser.add_argument('--wait', help='Whether exit only when the triggered build succeeds',
+              action='store_true')
     args = parser.parse_args()
 
     if args.failure:
         repo = nm_repo_map[ args.adapter ]
         trigger_travis_build( generate_failure_callback(), args.owner,repo)
     else:
-        trigger_travis_build( generate_travis_job(args.adapter, args.owner),
-                     args.owner, 'systemtests' )
+        if args.wait:
+            trigger_travis_and_wait_and_respond(generate_travis_job(args.adapter, args.owner, trigger_failure
+                = False), args.owner, 'systemtests' )
+        else:
+            trigger_travis_build( generate_travis_job(args.adapter, args.owner),
+                         args.owner, 'systemtests' )
