f40f84967c958c61f41594f08a3f7a8eca6aa26c
Benjamin Rüth <benjamin.rueth@tum.de>
2019-07-08 18:14:37 +0200

    remove default docker username to force user to provide it and avoid inconsistencies

--- build_base.py
+++ build_base.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Build local.')
     parser.add_argument('-d', '--dockerfile', type=str, help="Choose Dockerfile you want to build", default= "Dockerfile.Ubuntu1604.home")
     parser.add_argument('-b', '--branch', help="preCICE branch to use", default = "develop")
-    parser.add_argument('-u', '--docker_username', help="docker username", default = "precice")
-    parser.add_argument('-f', '--force_rebuild', nargs='+', help="Force rebuild of variable parts of docker image",
+    parser.add_argument('-u', '--docker-username', help="docker username")
+    parser.add_argument('-f', '--force-rebuild', nargs='+', help="Force rebuild of variable parts of docker image",
                         default = [], choices  = ["precice", "tests"])
     args = parser.parse_args()
     tag = args.docker_username.lower() + "/" + args.dockerfile.lower() + '-' + args.branch.lower() 
