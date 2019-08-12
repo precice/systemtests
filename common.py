53dd9d4ed6963486f93a08f706a8600c65b3cd3d
Dmytro Sashko <dmshko@gmail.com>
2019-08-12 22:48:22 +0200

    Add openFOAM nearest projection mapping test

--- common.py
+++ common.py
@@ -33,7 +33,11 @@ def get_test_variants(test_name, root = os.getcwd()):
     """ List of all available system tests """
     prefixes = ["Test_", "TestCompose_"]
     test_variants = [s[len(prefix):]  for prefix in prefixes for s in os.listdir(root)
-            if s.startswith(prefix +test_name)]
+            if s.startswith(prefix + test_name)]
+    # filter cases with same participants
+    if test_name and not "_" in test_name:
+        test_variants = [t for t in test_variants if not "_" in t]
+
     return test_variants
 
 
@@ -41,7 +45,8 @@ def get_test_participants(test_name):
     """ Returns solvers that participate in the test """
     solvers_abbr = {"ccx": "calculix-adapter", "su2": "su2-adapter", "of": "openfoam-adapter", 
             "dealii":"dealii-adapter", "bindings": "bindings"}
-    return [solvers_abbr[abbr] for abbr in test_name.lower().split('-')]
+
+    return [solvers_abbr[abbr] for abbr in  test_name.lower().split('_')[0].split('-')]
 
 
 from contextlib import contextmanager
