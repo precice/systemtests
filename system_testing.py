931cf9097c2cdd6ef76a4739c40561645cd04223
Konrad Eder <konrad.alexander@tum.de>
2019-09-25 17:26:30 +0200

    change elastictube1d to classic docker test from docker-compose

--- system_testing.py
+++ system_testing.py
@@ -148,7 +148,7 @@ def build_run_compare(test, tag, branch, local_precice, force_rebuild, rm_all):
     """ Runs and compares test, using precice branch. """
 
     compose_tests = ["dealii-of", "of-of", "su2-ccx", "of-ccx", "of-of_np",
-            "fe-fe","nutils-of", "of-ccx_fsi", "et1d"]
+            "fe-fe","nutils-of", "of-ccx_fsi"]
     test_basename = test.split(".")[0]
     if local_precice:
         build_adapters(test_basename, tag, branch, local_precice, force_rebuild)
