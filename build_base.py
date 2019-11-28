fabdc4def2780ae1df2bed105dc1a3614bb8f172
BenjaminRueth <benjamin.rueth@tum.de>
2019-11-28 20:15:58 +0100

    add argument to switch petsc on/off

--- build_base.py
+++ build_base.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Build local.')
     parser.add_argument('-d', '--dockerfile', type=str, help="Choose Dockerfile you want to build", default= "Dockerfile.Ubuntu1604.home")
     parser.add_argument('-b', '--branch', help="preCICE branch to use", default = "develop")
+    parser.add_argument('-p', '--petsc', help="set 'yes', if you want to build with PETSc.", default = "no", choices={'yes', 'no'})
     parser.add_argument('-u', '--docker-username', help="docker username")
     parser.add_argument('-f', '--force-rebuild', nargs='+', help="Force rebuild of variable parts of docker image",
                         default = [], choices  = ["precice", "tests"])
@@ -12,7 +13,6 @@ if __name__ == "__main__":
     tag = args.docker_username.lower() + "/" + args.dockerfile.lower() + '-' + args.branch.lower() 
     docker.build_image(tag = tag,
                        dockerfile = args.dockerfile,
-                       build_args = {"branch" : args.branch},
-                       force_rebuild = args.force_rebuild,
-                       namespace = "")
+                       build_args = {"branch" : args.branch, "petsc_para" : args.petsc},
+                       force_rebuild = args.force_rebuild)
                        
