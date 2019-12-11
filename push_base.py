3ce9d014bb2687a9eecfe53c492ab734189677f9
Rüth <ga25zih@mytum.d>
2019-12-11 13:55:31 +0100

    Add script for pushing

---
+++ push_base.py
@@ -0,0 +1,17 @@
+import argparse, docker
+import system_testing
+import os
+                           
+if __name__ == "__main__":
+    # Parsing flags
+    parser = argparse.ArgumentParser(description='Build local.')
+    parser.add_argument('-d', '--dockerfile', type=str, help="Choose Dockerfile you want to build", default="precice/Dockerfile.Ubuntu1604.home")
+    parser.add_argument('-b', '--branch', help="preCICE branch to use", default="develop")
+    parser.add_argument('-p', '--petsc', help="set 'yes', if you want to build with PETSc.", default="no", choices={'yes', 'no'})
+    parser.add_argument('-u', '--docker-username', help="docker username", default=os.environ["DOCKER_USERNAME"])
+
+    args = parser.parse_args()
+    tag = system_testing.compose_tag(args.docker_username, args.dockerfile, args.branch, args.petsc)
+    docker.push_image(tag=tag,
+                      namespace="")
+                       
