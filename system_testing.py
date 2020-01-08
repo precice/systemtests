56ba8af122ad5e00002cfb4e51ffb0d66d98c671
Benjamin Rüth <benjamin.rueth@tum.de>
2020-01-08 16:58:43 +0100

    Filter away None

--- system_testing.py
+++ system_testing.py
@@ -195,6 +195,7 @@ def compose_tag(docker_username, base, features, branch):
     features_list.append(features.get("installation"))
     features_list.append("petsc") if (features.get("petsc") is "yes") else None
     features_list.append("mpich") if (features.get("mpich") is "yes") else None
+    features_list = list(filter(None, features_list))  # filter "None" features that might have been added by dict.get(key), if key did not exist.
 
     if features_list:  # list of features is not empty
         tag = docker_username.lower() + "/" + base + "-" + ".".join(features_list).lower() + '-' + branch.lower()
