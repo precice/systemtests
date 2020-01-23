a6ae55cf14bc09d90cdf38883ef0af2a97fa4e64
Eder-K <konrad.alexander.eder@tum.de>
2020-01-23 12:03:36 +0100

    Add optional debug argument

--- silent_compose.sh
+++ silent_compose.sh
@@ -6,41 +6,44 @@
 taillen=500
 failed=0
 
-# docker-compose up
-docker-compose up -d || exit 1
-services=($(docker-compose ps | awk '{print $1}'| tail -n +3))
+if [ "$1" = "debug" ]; then
+	docker-compose up
+else
+	docker-compose up -d || exit 1
+	services=($(docker-compose ps | awk '{print $1}'| tail -n +3))
 
-# maximum test timeout is 10 minutes
-for i in {1..10}; do
-  for service in ${services[@]}; do
-    # check if the service is still running
-    if [ -z "$(docker ps | grep $service | cut -d ' ' -f 1)" ]; then
-      if [ "$(docker inspect $service --format='{{.State.ExitCode}}')" -eq 1 ]; then
-        echo "$service failed! Find the logs below"
-        failed=1
-        docker-compose logs --tail=$taillen  $service
-      fi
-      # pop
-      services=(${services[@]/$service})
-    fi
-  done
-  # exit if nothing is running
-  if [ ${#services[@]} -eq 0 ]; then
-    echo "All adapters finished!"
-    exit $failed
-  fi
-  echo "Running the simulation...Be patient"
-  sleep 60
-done
+	# maximum test timeout is 10 minutes
+	for i in {1..10}; do
+	  for service in ${services[@]}; do
+	    # check if the service is still running
+	    if [ -z "$(docker ps | grep $service | cut -d ' ' -f 1)" ]; then
+	      if [ "$(docker inspect $service --format='{{.State.ExitCode}}')" -eq 1 ]; then
+	        echo "$service failed! Find the logs below"
+	        failed=1
+	        docker-compose logs --tail=$taillen  $service
+	      fi
+	      # pop
+	      services=(${services[@]/$service})
+	    fi
+	  done
+	  # exit if nothing is running
+	  if [ ${#services[@]} -eq 0 ]; then
+	    echo "All adapters finished!"
+	    exit $failed
+	  fi
+	  echo "Running the simulation...Be patient"
+	  sleep 60
+	done
 
-echo "Timeout!"
-echo "Printing logs for services:"
+	echo "Timeout!"
+	echo "Printing logs for services:"
 
-# probably failed due to communication issues,
-# list individual logs and exit
-for service in ${services[@]}; do
-    docker-compose logs --tail=$taillen $service
-    docker-compose stop $service
-done
+	# probably failed due to communication issues,
+	# list individual logs and exit
+	for service in ${services[@]}; do
+	    docker-compose logs --tail=$taillen $service
+	    docker-compose stop $service
+	done
 
-exit 1
+	exit 1
+fi
