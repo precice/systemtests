17393ff9659d507c789f4bf6ada51fef336886cb
Eder-K <konrad.alexander.eder@tum.de>
2020-01-25 01:11:59 +0100

    Add log pushing

--- push.py
+++ push.py
@@ -1,5 +1,5 @@
 """
-!!!!!!!!!!!!!!!!! WIP !!!!!!!!!!!!!!!!!!!!
+### WIP ###
 Script for pushing travis internal files to a github repository.
 
 This script pushes to: https://github.com/precice/precice_st_output.
@@ -44,13 +44,6 @@ import inspect
 #
 #     return commit
 #
-# def get_travis_job_log(job_id, tail = 100):
-#
-#     txt_url = "https://api.travis-ci.org/v3/job/{}/log.txt".format(job_id)
-#     req = Request(txt_url)
-#     response = urlopen( req ).read().decode()
-#     job_log = "\n".join( response.split("\n")[-tail:] )
-#     return job_log
 #
 # def create_job_log(test, log, exit_status):
 #
@@ -113,143 +106,162 @@ import inspect
 #             ccall("git rm -rf {}".format(output_log_dir))
 #         ccall("mv {} {}".format(output_dir, output_log_dir))
 #         ccall("git add .")
-#
+# #
 # def add_job_log(systest, failed, log_dir):
 #     with chdir(log_dir):
 #         log_name = "log_{test}.md".format(test = systest)
 #         with open(log_name, "w") as log:
 #             create_job_log(systest, log, failed)
 #         ccall("git add {log_name}".format(log_name = log_name))
-#
-#
-# def generate_commit_message(output_dir, success, test, base):
-#
-#     travis_build_number = os.environ["TRAVIS_BUILD_NUMBER"]
-#     travis_build_web_url = os.environ["TRAVIS_BUILD_WEB_URL"]
-#     travis_job_number = os.environ["TRAVIS_JOB_NUMBER"]
-#     travis_job_web_url = os.environ["TRAVIS_JOB_WEB_URL"]
-#     commit_msg_lines = []
-#
-#     if success:
-#         commit_msg_lines = ["Success Job: {}".format(travis_job_number)]
-#     else:
-#         commit_msg_lines = ["Failure Job: {}".format(travis_job_number)]
-#         # folder with output was not created, we probably failed before producing
-#         # any of the results
-#         if not os.path.isdir(output_dir):
-#             commit_msg_lines += ["[Failed to produce results]"]
-#
-#     commit_msg_lines += ["Base: {}".format(base)]
-#     commit_msg_lines += ["Test: {}".format(test)]
-#     commit_msg_lines += ["Build url: {}".format(travis_build_web_url)]
-#     commit_msg_lines += ["Job url: {}".format(travis_job_web_url)]
-#
-#     return commit_msg_lines
+
+
+def generate_commit_message(output_dir, success, test, base):
+
+    travis_build_number = os.environ["TRAVIS_BUILD_NUMBER"]
+    travis_build_web_url = os.environ["TRAVIS_BUILD_WEB_URL"]
+    travis_job_number = os.environ["TRAVIS_JOB_NUMBER"]
+    travis_job_web_url = os.environ["TRAVIS_JOB_WEB_URL"]
+    commit_msg_lines = []
+
+    if success:
+        commit_msg_lines = ["Success Job: {}".format(travis_job_number)]
+    else:
+        commit_msg_lines = ["Failure Job: {}".format(travis_job_number)]
+        # folder with output was not created, we probably failed before producing
+        # any of the results
+        if not os.path.isdir(output_dir):
+            commit_msg_lines += ["[Failed to produce results]"]
+
+    commit_msg_lines += ["Base: {}".format(base)]
+    commit_msg_lines += ["Test: {}".format(test)]
+    commit_msg_lines += ["Build url: {}".format(travis_build_web_url)]
+    commit_msg_lines += ["Job url: {}".format(travis_job_web_url)]
+
+    return commit_msg_lines
+
+def get_travis_job_log(job_id, tail = 0):
+
+    txt_url = "https://api.travis-ci.org/v3/job/{}/log.txt".format(job_id)
+    req = Request(txt_url)
+    response = urlopen( req ).read().decode()
+
+    # if log cutoff is enabled
+    if tail > 0:
+        job_log = "\n".join( response.split("\n")[-tail:] )
+        else
+        job_log = response
+
+        return job_log
 
 tag_descriptions = {
     'no_output' : "\n- **This test generated no Output files!** These should normally be stored in the folder '/Output' located at the root of the 'tutorial-data' container, but no files were found there.\n",
     'allowed_failure' : "\n- This test has been marked as allowed failure.\n"
 }
 
-def write_readme(*tags):
+def add_readme(job_path, job_result):
     """
     Create a README.md at the location specified by readme_path.
     """
-    tags = list(tags)
     job_link = os.environ["TRAVIS_JOB_WEB_URL"]
     job_name = os.environ["TRAVIS_JOB_NAME"]
-    job_status = "Success" if (os.environ["TRAVIS_TEST_RESULT"] == 0) else "Failure"
+    job_status = "Success" if job_result else "Failure"
 
     readme_text = """
-                  # {name}
+    # {name}
 
-                  Job Status: **{status}**
+    Job Status: **{status}**
 
-                  [Link to Job page on TravisCI]({link})
+    [Link to Job page on TravisCI]({link})
 
-                  ---
-                  """.format(name=job_name, status=job_status, link=job_link)
+    ---
+    """.format(name=job_name, status=job_status, link=job_link)
 
     readme_text = inspect.cleandoc(readme_text) # cleans indentation
-
-    # If any tags were supplied, provide addition information
-    if tags:
-        readme_text += "\n### Additional Information:\n"
-        unknown_tags = []
-        for tag in tags:
-            # check if desciption exists for current tag
-            if tag in tag_descriptions:
-              readme_text += tag_descriptions[tag]
-            else:
-              unknown_tags.append(tag)
-
-        if len(unknown_tags) > 0:
-            readme_text += "\n- Further keywords: {tags}".format(tags=unknown_tags)
-
-    return readme_text
-
-def store_readme(readme_text, readme_path):
-    ccall("echo '{text}' > {path}".format(text=readme_text, path=readme_path))
-
+    with chdir(job_path):
+        with open("README.md", "w") as readme:
+            readme.write(readme_txt)
+        ccall("git add README.md")
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Push results from containers to output repository')
     parser.add_argument('-t', '--test', help="Choose systemtest, results of which to push")
-    parser.add_argument('-s', '--success', action='store_true' ,help="If test was successful")
     parser.add_argument('-b', '--base', type=str, help="Base image of the test", default="Ubuntu1604.home")
+    parser.add_argument('--st-branch', type=str, help="Branch of precice_st_output to push to", default="EderK-push_files")
     args = parser.parse_args()
-
 
     repo_folder = "precice_st_output"
     build_folder = os.environ["TRAVIS_BUILD_NUMBER"]
     job_folder = os.environ["TRAVIS_JOB_NUMBER"]
+    job_id = os.environ["TRAVIS_JOB_ID"]
+    job_result = os.environ["TRAVIS_TEST_RESULT"]
+    job_success = True if (job_result == 0) else False
 
-    # TODO: change this to master branch when merging
-    ccall("git clone -b EderK-push_files https://github.com/precice/precice_st_output")
+    # TODO: change default to master branch when merging
+    ccall("git clone -b {st_branch} https://github.com/precice/precice_st_output".\
+        format(st_branch=args.st_branch))
 
+    # Path to repository folder
     repo_path = os.path.join(os.getcwd(), repo_folder)
-    file_path = os.path.join(os.getcwd(), repo_folder, build_folder, job_folder)
+    # Path to job folder
+    job_path = os.path.join(os.getcwd(), repo_folder, build_folder, job_folder)
+    # Path to Output folder inside a job folder
     output_path = os.path.join(file_path, "Output")
+    # Path to Logs folder inside a job folder
+    logs_path = os.path.join(file_path, "Logs")
 
-    ccall("mkdir -p {}".format(file_path))
+    # Dont need to mkdir, will be done by docker cp
+    # ccall("mkdir -p {}".format(logs_path))
+    # ccall("mkdir -p {}".format(output_path))
 
     # extract files from container
     ccall("docker cp tutorial-data:/Output {}".format(file_path))
+    # move container logs into correct folder
+    ccall("docker cp tutorial-data:/Logs {}".format(file_path))
+
+    # create travis log
+    with chdir(logs_path):
+        with open("travis-log.md", "w") as log:
+            log.write('```\n{log}\n```\n'.format(log=get_travis_job_log(job_id)))
+
+    # create README
+    add_readme(job_path, job_success)
+
 
     # Check if Output directory is empty. If yes, include a small README
     no_output = False
     if not os.listdir(output_path):
-        ccall("echo '# No Output files found!' > {path}".format(path=
+        ccall("echo '# No output files found!' > {path}".format(path=
         os.path.join(output_path, "README.md")))
         no_output = True
-
-    # Store README with general job info in main folder
-    readme_path = os.path.join(file_path, 'README.md')
-    readme_txt = write_readme('no_output' if no_output else 'output',
-                              'test-tag1',
-                              'test-tag2')
-
-    store_readme(readme_txt, readme_path)
+    # Check if Logs directory is empty. If yes, include a small README
+    no_logs = False
+    if not os.listdir(logs_path):
+        ccall("echo '# No log files found!' > {path}".format(path=
+        os.path.join(output_path, "README.md")))
+        no_output = True
 
     os.chdir(repo_path)
     with chdir(repo_path):
         ccall("git add {}".format(file_path))
 
     # finally commit
-    commit_msg = "Job Success" if args.success else "Job Failure"
-    # Use special commit message if no Output generated
+    commit_msg = "Job Success" if job_success else "Job Failure"
     if no_output:
-        commit_msg += ", NO OUTPUT!"
+        commit_msg += ", NO OUTPUT"
+    if no_logs:
+        commit_msg += ", NO LOGS"
     ccall("git commit -m '{}'".format(commit_msg))
     ccall("git config user.name 'Precice Bot'")
     ccall("git config user.email ${PRECICE_BOT_EMAIL}")
     ccall("git remote set-url origin https://${GH_TOKEN}@github.com/precice/precice_st_output.git > /dev/null 2>&1")
 
+
     failed_push_count = 0
+    failed_push_limit = 50
     # push, retry if failed
     while call("git push"):
         ccall("git pull --rebase")
         failed_push_count += 1
-        if failed_push_count >= 50:
+        if failed_push_count >= failed_push_limit:
             break
